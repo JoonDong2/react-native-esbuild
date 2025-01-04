eef308de8a64d27992df0d9901986fd8d10c94b1
Joondong <kwonjoondong@gmail.com>
2025-01-04 11:34:08 +0900

    feat: start `/index.bundle`

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,35 +1,61 @@
-import { createServer } from 'vite';
 import { type Config } from '@react-native-community/cli-types';
 import { type StartArguments } from './types';
 
-export async function start(_: string[], config: Config, __: StartArguments) {
+import express from 'express';
+import BuildContext from '../utils/BuildContext';
+import { type Platform } from '../constants/platform';
+
+interface BundleQuery {
+  platform: Platform;
+  dev: 'true' | 'false';
+  minify: 'true' | 'false';
+  // not used
+  lazy: 'true' | 'false';
+  app: string;
+  modulesOnly: 'true' | 'false';
+  runModule: 'true' | 'false';
+  excludeSource: 'true' | 'false';
+  sourcePaths: 'url-server' | 'absolute';
+}
+
+export async function start(_: string[], config: Config, args: StartArguments) {
   const { root } = config;
+  const { host = '127.0.0.1', port = 8081 } = args;
 
-  const define = {
-    '__DEV__': 'true',
-    'window': 'globalThis', // JSC와 hermes에 정의 안되어 있다.
-    'global': 'globalThis', // JSC와 hermes에 정의 안되어 있다.
-    'process.env.NODE_ENV': `"development"`,
-  };
+  const devServer = express();
 
-  const server = await createServer({
-    define,
-    server: {
-      hmr: {
-        port: 8081,
-      },
-      watch: {
-        cwd: root,
-      },
-    },
-    plugins: [],
+  devServer.get('/status', (_, res) => {
+    res.status(200).send('packager-status:running');
   });
 
-  await server.listen();
+  const buildContexts: {
+    [key in Platform]?: BuildContext;
+  } = {};
+
+  devServer.get('/index.bundle', async (req, res) => {
+    const { platform, dev, minify } = req.query as unknown as BundleQuery;
+    let buildContext = buildContexts[platform];
+    if (!buildContext) {
+      buildContext = await BuildContext.create({
+        root,
+        platform,
+        dev: dev === 'true',
+        minify: minify === 'true',
+        write: false,
+        sourcemap: 'inline',
+      });
+      buildContexts[platform] = buildContext;
+    }
+
+    const result = await buildContext.build();
+    res.status(200).send(result.outputFiles?.[0]?.text);
+  });
+
+  devServer.listen(port, host, () => {
+    console.log(`Dev server running on ${host}:${port}`);
+  });
 
   return {
-    stop: () => {
-      server.close();
-    },
+    stop: () => {},
   };
 }
