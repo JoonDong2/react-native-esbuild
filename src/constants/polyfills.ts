ed0b2c9246699b2e22f4226f0233cb5b4c8e2cfe
Joondong <kwonjoondong@gmail.com>
2025-01-04 11:58:46 +0900

    chore: extract `mergeConfig` from `vite`, remove `vite`

--- src/constants/polyfills.ts
+++ src/constants/polyfills.ts
@@ -1,16 +1,16 @@
 import esbuild, { type BuildOptions } from 'esbuild';
-import { mergeConfig } from 'vite';
+import { mergeConfig } from '../utils/config';
 
 export const getJsPolyfills = async (options: BuildOptions = {}) => {
   return (
     await esbuild.build(
-      mergeConfig<BuildOptions, BuildOptions>(options, {
+      mergeConfig(options, {
         outdir: 'node_modules/.cache/react-native-esbuild/',
         entryPoints: require('@react-native/js-polyfills')(),
         sourcemap: false,
         write: false,
         format: 'iife',
-      }) as BuildOptions
+      })
     )
   ).outputFiles!.reduce((acc, cur) => {
     return acc + cur.text;
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,4 @@
 import type { BuildOptions } from 'esbuild';
-import { mergeConfig as mergeConfigOrigin } from 'vite';
 
 export const getConfig = (path: string) => {
   try {
@@ -8,6 +7,165 @@ export const getConfig = (path: string) => {
     return {};
   }
 };
+
+function isObject(value: unknown): value is Record<string, any> {
+  return Object.prototype.toString.call(value) === '[object Object]';
+}
+
+interface Alias {
+  find: string | RegExp;
+  replacement: string;
+  /**
+   * Instructs the plugin to use an alternative resolving algorithm,
+   * rather than the Rollup's resolver.
+   * @default null
+   */
+  customResolver?: any;
+}
+
+type AliasOptions = readonly Alias[] | { [find: string]: string };
+
+// https://github.com/vitejs/vite/issues/1363
+// work around https://github.com/rollup/plugins/issues/759
+function normalizeSingleAlias({
+  find,
+  replacement,
+  customResolver,
+}: Alias): Alias {
+  if (
+    typeof find === 'string' &&
+    find[find.length - 1] === '/' &&
+    replacement[replacement.length - 1] === '/'
+  ) {
+    find = find.slice(0, find.length - 1);
+    replacement = replacement.slice(0, replacement.length - 1);
+  }
+
+  const alias: Alias = {
+    find,
+    replacement,
+  };
+  if (customResolver) {
+    alias.customResolver = customResolver;
+  }
+  return alias;
+}
+
+function mergeAlias(
+  a?: AliasOptions,
+  b?: AliasOptions
+): AliasOptions | undefined {
+  if (!a) return b;
+  if (!b) return a;
+  if (isObject(a) && isObject(b)) {
+    return { ...a, ...b };
+  }
+  // the order is flipped because the alias is resolved from top-down,
+  // where the later should have higher priority
+  return [...normalizeAlias(b), ...normalizeAlias(a)];
+}
+
+function normalizeAlias(o: AliasOptions = []): Alias[] {
+  return Array.isArray(o)
+    ? o.map(normalizeSingleAlias)
+    : Object.keys(o).map((find) =>
+        normalizeSingleAlias({
+          find,
+          replacement: (o as any)[find],
+        })
+      );
+}
+
+function backwardCompatibleWorkerPlugins(plugins: any) {
+  if (Array.isArray(plugins)) {
+    return plugins;
+  }
+  if (typeof plugins === 'function') {
+    return plugins();
+  }
+  return [];
+}
+
+function arraify<T>(target: T | T[]): T[] {
+  return Array.isArray(target) ? target : [target];
+}
+
+function mergeConfigRecursively(
+  defaults: Record<string, any>,
+  overrides: Record<string, any>,
+  rootPath: string
+) {
+  const merged: Record<string, any> = { ...defaults };
+  for (const key in overrides) {
+    const value = overrides[key];
+    if (value == null) {
+      continue;
+    }
+
+    const existing = merged[key];
+
+    if (existing == null) {
+      merged[key] = value;
+      continue;
+    }
+
+    // fields that require special handling
+    if (key === 'alias' && (rootPath === 'resolve' || rootPath === '')) {
+      merged[key] = mergeAlias(existing, value);
+      continue;
+    } else if (key === 'assetsInclude' && rootPath === '') {
+      merged[key] = [].concat(existing, value);
+      continue;
+    } else if (
+      key === 'noExternal' &&
+      (rootPath === 'ssr' || rootPath === 'resolve') &&
+      (existing === true || value === true)
+    ) {
+      merged[key] = true;
+      continue;
+    } else if (key === 'plugins' && rootPath === 'worker') {
+      merged[key] = () => [
+        ...backwardCompatibleWorkerPlugins(existing),
+        ...backwardCompatibleWorkerPlugins(value),
+      ];
+      continue;
+    } else if (key === 'server' && rootPath === 'server.hmr') {
+      merged[key] = value;
+      continue;
+    }
+
+    if (Array.isArray(existing) || Array.isArray(value)) {
+      merged[key] = [...arraify(existing), ...arraify(value)];
+      continue;
+    }
+    if (isObject(existing) && isObject(value)) {
+      merged[key] = mergeConfigRecursively(
+        existing,
+        value,
+        rootPath ? `${rootPath}.${key}` : key
+      );
+      continue;
+    }
+
+    merged[key] = value;
+  }
+  return merged;
+}
+
+function _mergeConfig<
+  D extends Record<string, any>,
+  O extends Record<string, any>,
+>(
+  defaults: D extends Function ? never : D,
+  overrides: O extends Function ? never : O,
+  isRoot = true
+): Record<string, any> {
+  if (typeof defaults === 'function' || typeof overrides === 'function') {
+    throw new Error(`Cannot merge config in form of callback`);
+  }
+
+  return mergeConfigRecursively(defaults, overrides, isRoot ? '' : '.');
+}
 
 export const mergeConfig = (...configs: BuildOptions[]): BuildOptions => {
   if (!Array.isArray(configs) || configs.length === 0) {
@@ -22,6 +180,6 @@ export const mergeConfig = (...configs: BuildOptions[]): BuildOptions => {
   const remains = configs.slice(1)!;
 
   return remains.reduce<BuildOptions>((acc, cur) => {
-    return mergeConfigOrigin<BuildOptions, BuildOptions>(acc, cur);
+    return _mergeConfig<BuildOptions, BuildOptions>(acc, cur);
   }, first);
 };
