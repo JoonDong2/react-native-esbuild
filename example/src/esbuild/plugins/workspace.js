f1269a4d6c9aa76ec3f0e327cf51bf1592e9486e
Joondong <kwonjoondong@gmail.com>
2025-01-05 19:46:04 +0900

    fix: example/plugins/esbuild/workspace - ensure only "react" and "react-native" as singleton → resovle "react-native-esbuild"

--- example/src/esbuild/plugins/workspace.js
+++ example/src/esbuild/plugins/workspace.js
@@ -5,26 +5,21 @@ const workspace = () => {
     name: 'resolve-workspaces',
     setup(build) {
       build.onResolve({ filter: /.*/ }, (args) => {
-        // 상대 경로나 절대 경로는 기본 해석 방식 사용
-        if (args.path.startsWith('.') || path.isAbsolute(args.path)) {
-          return;
+        if (args.path === 'react-native-esbuild') {
+          return {
+            path: require.resolve(path.join(__dirname, '../../../../')),
+          };
         }
 
-        // 현재 프로젝트와 Workspaces 루트의 node_modules를 순서대로 검색
-        const currentProjectNodeModules = path.resolve(
-          __dirname,
-          'node_modules'
-        );
-        const workspaceRootNodeModules = path.resolve(
-          __dirname,
-          '../../node_modules'
-        );
+        if (args.path === 'react-native' || args.path === 'react') {
+          const nodeModules = path.resolve(__dirname, '../../../node_modules');
 
-        return {
-          path: require.resolve(args.path, {
-            paths: [currentProjectNodeModules, workspaceRootNodeModules],
-          }),
-        };
+          return {
+            path: require.resolve(args.path, {
+              paths: [nodeModules],
+            }),
+          };
+        }
       });
     },
   };
